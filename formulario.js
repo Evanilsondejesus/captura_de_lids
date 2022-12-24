67f3ff02bb9bfad57eeaf99d91e02cd9b9ae0f16
evanilon de jesus <evaevanilson@gmail.com>
2022-12-24 18:25:45 -0300

    first commit

---
+++ formulario.js
@@ -0,0 +1,49 @@
+  // Your web app's Firebase configuration
+  
+
+     // Your web app's Firebase configuration
+     const firebaseConfig = {
+      apiKey: "AIzaSyBm02OzGxD_NkLRe7LDitezeYOnqR-9g7I",
+  authDomain: "plataforma-evangelica.firebaseapp.com",
+  databaseURL: "https://plataforma-evangelica-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "plataforma-evangelica",
+  storageBucket: "plataforma-evangelica.appspot.com",
+  messagingSenderId: "516992557622",
+  appId: "1:516992557622:web:0a8c314edc91c9265eee97",
+  measurementId: "G-3DJ6T16GZE"
+      };
+      
+      
+            
+            firebase.initializeApp(firebaseConfig);
+      
+      
+          
+      
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+        //window.open("pag2.html")
+    
+
+         
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
